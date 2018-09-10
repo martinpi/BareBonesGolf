9a088605e86fb77911621faba1a792a98f0a2c1a
Martin Pichlmair <pi@brokenrul.es>
2018-09-10 14:56:10 +0200

    - set up initial scene

---
+++ BareBonesGolf/Assets/Scripts/DragKick.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class DragKick : MonoBehaviour {
+
+	public Vector3 _dragPos;
+
+	public float force = 5f;
+
+	void OnDrawGizmos() {
+		if (Input.GetMouseButton(0)) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawLine(transform.position, _dragPos);
+		}
+	}
+
+    void Update() {
+		if (Input.GetMouseButton(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast(ray, out hit, 100F)) {
+				_dragPos = hit.point;
+			}
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			Vector3 dist = transform.position - _dragPos;
+			dist.y = 0f;
+			GetComponent<Rigidbody>().AddForce(dist.normalized * force, ForceMode.Impulse);
+		}
+    }
+
+}
